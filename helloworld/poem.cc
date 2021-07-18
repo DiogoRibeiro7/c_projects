e07c63088527276090ef10904a99432d23e96e34
DiogoRibeiro7 <diogo_dj@hotmail.com>
2021-07-18 01:27:56 +0100

    just fun

--- helloworld/poem.cc
+++ helloworld/poem.cc
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-/**
+/*
  * a simple program for demostranting the badic of C++ project.
  * 
  * It does a good job demonstrating C++ Fundamentels 
+ * but a terribel job with poetry.
+ */
+ 
+ int main() {
+     cout << "Don't you just feel like a louse";
+     cout << endl;
+
+     cout << "To learn that your \"new\" theorem was proven by Gauss?";
+     cout << endl;
+     
+     return 0;
+ }
